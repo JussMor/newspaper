d03ff553d5b135698bc08a9da358c2cc3baaf405
Junior Moreira <jussmor@outlook.es>
2024-06-08 14:47:54 -0500

    fix: select on in table

--- lib/newspaper_web/components/core_components.ex
+++ lib/newspaper_web/components/core_components.ex
@@ -115,7 +115,7 @@ defmodule NewspaperWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-999 rounded-lg p-3 ring-1 z-9999",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
@@ -455,73 +455,75 @@ defmodule NewspaperWeb.CoreComponents do
         <:col :let={user} label="username"><%= user.username %></:col>
       </.table>
   """
-  attr :id, :string, required: true
-  attr :rows, :list, required: true
-  attr :row_id, :any, default: nil, doc: "the function for generating the row id"
-  attr :row_click, :any, default: nil, doc: "the function for handling phx-click on each row"
+attr :id, :string, required: true
+attr :rows, :list, required: true
+attr :row_id, :any, default: nil, doc: "the function for generating the row id"
+attr :row_click, :any, default: nil, doc: "the function for handling phx-click on each row"
 
-  attr :row_item, :any,
-    default: &Function.identity/1,
-    doc: "the function for mapping each row before calling the :col and :action slots"
+attr :row_item, :any,
+  default: &Function.identity/1,
+  doc: "the function for mapping each row before calling the :col and :action slots"
 
-  slot :col, required: true do
-    attr :label, :string
-  end
+slot :col, required: true do
+  attr :label, :string
+end
 
-  slot :action, doc: "the slot for showing user actions in the last table column"
+slot :action, doc: "the slot for showing user actions in the last table column"
 
-  def table(assigns) do
-    assigns =
-      with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
-        assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
-      end
+def table(assigns) do
+  assigns =
+    with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
+      assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
+    end
 
-    ~H"""
-    <div class="overflow-x-auto   bg-white shadow">
-      <table class="min-w-full">
-        <thead class="text-sm text-left leading-6 text-zinc-500">
-          <tr>
-            <th :for={col <- @col} class="py-4 px-6 font-normal"><%= col[:label] %></th>
-            <th :if={@action != []} class="relative p-0 pb-4">
-              <span class="sr-only"><%= gettext("Actions") %></span>
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
-        >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative ", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 px-6">
-                <span class="absolute  right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 px-6 text-right text-sm font-medium">
-                <span class="absolute  -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    """
-  end
+
+  ~H"""
+  <div class="overflow-x-auto bg-white shadow">
+    <table class="min-w-full">
+      <thead class="text-sm text-left leading-6 text-zinc-500">
+        <tr>
+          <th :for={col <- @col} class="py-4 px-6 font-normal"><%= col[:label] %></th>
+          <th :if={@action != []} class="relative p-0 pb-4">
+            <span class="sr-only"><%= gettext("Actions") %></span>
+          </th>
+        </tr>
+      </thead>
+      <tbody
+        id={@id}
+        phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
+        class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
+      >
+        <tr :for={{id, item} <- @rows} id={@row_id && @row_id.({id, item})} class="group hover:bg-zinc-50">
+          <td
+            :for={{col, i} <- Enum.with_index(@col)}
+            phx-click={@row_click && @row_click.({id, item})}
+            class={["relative ", @row_click && "hover:cursor-pointer"]}
+          >
+            <div class="block py-4 px-6">
+              <span class="absolute right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
+              <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
+                <%= render_slot(col, @row_item.({id, item})) %>
+              </span>
+            </div>
+          </td>
+          <td :if={@action != []} class="relative w-14 p-0">
+            <div class="relative whitespace-nowrap py-4 px-6 text-right text-sm font-medium">
+              <span class="absolute -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
+              <span
+                :for={action <- @action}
+                class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
+              >
+                <%= render_slot(action, @row_item.({id, item})) %>
+              </span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  """
+end
+
 
   @doc """
   Renders a data list.
--- lib/newspaper_web/live/settings_live/roles_live/index.ex
+++ lib/newspaper_web/live/settings_live/roles_live/index.ex
@@ -14,11 +14,18 @@ defmodule NewspaperWeb.RolesLive.Index do
           <.link patch={~p"/settings/roles/new"}>
             <.button>New Role</.button>
           </.link>
+          <.button phx-click="delete_selected" phx-disable-with="Deleting...">Delete Selected</.button>
         </:actions>
       </.header>
+
+      <%= if @flash do %>
+        <div class="flash-message">
+          <%= live_flash(@flash, :info) %>
+        </div>
+      <% end %>
     </div>
 
-
+    <% check_all_html = ~s(<input type="checkbox" name="toggle-all" id="toggle-all" phx-click="toggle-all" phx-update="ignore" />) %>
 
     <div class="ml-6 sm:mx-6 mt-11 grid grid-cols-1 grid-rows-1 grid-flow-row-dense gap-6">
       <.table
@@ -26,6 +33,13 @@ defmodule NewspaperWeb.RolesLive.Index do
         rows={@streams.roles}
         row_click={fn {_id, role} -> JS.navigate(~p"/settings/roles/#{role}") end}
       >
+        <:col :let={{_id, role}} label={raw(check_all_html)}>
+         <% checked = if (role.id in @toggle_ids), do: true, else: false %>
+            <% IO.inspect( @toggle_ids)%>
+          <input type="checkbox" name="toggle"
+            phx-click="toggle" phx-value-toggle-id={role.id} checked={checked} />
+
+        </:col>
         <:col :let={{_id, role}} label="Role"><%= role.name %></:col>
         <:col :let={{_id, role}} label="Description"><%= role.description %></:col>
         <:action :let={{_id, role}}>
@@ -45,33 +59,37 @@ defmodule NewspaperWeb.RolesLive.Index do
       </.table>
     </div>
 
-     <.offcanvas title="Listing Lasnews" subtitle="List of lasnews" id="roles-offcanvas"
-     :if={@live_action in [:new, :edit]} show on_cancel={JS.patch(~p"/settings/roles")}>
-        <.live_component
-          module={NewspaperWeb.RolesLive.FormComponent}
-          id={@role.id || :new}
-          title={@page_title}
-          action={@live_action}
-          role={@role}
-          patch={~p"/settings/roles"}
-        />
-     </.offcanvas>
-
-
+    <.offcanvas title="Listing Roles" subtitle="List of roles" id="roles-offcanvas"
+      :if={@live_action in [:new, :edit]} show on_cancel={JS.patch(~p"/settings/roles")}>
+      <.live_component
+        module={NewspaperWeb.RolesLive.FormComponent}
+        id={@role.id || :new}
+        title={@page_title}
+        action={@live_action}
+        role={@role}
+        patch={~p"/settings/roles"}
+      />
+    </.offcanvas>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    roles = Roles.list_roles()
-    IO.inspect(roles)
-    {:ok, stream(socket, :roles, roles)}
+    {:ok, socket}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+
+    {:noreply,
+      socket
+      |> apply_action(socket.assigns.live_action, params)
+      |> stream(:roles, list_roles())
+      |> assign(:toggle_ids, [])
+      |> assign(:all_roles, list_roles())
+    }
   end
+
 
   defp apply_action(socket, :new, _params) do
     socket
@@ -96,11 +114,67 @@ defmodule NewspaperWeb.RolesLive.Index do
     {:noreply, stream_insert(socket, :roles, role)}
   end
 
+
+
+  def handle_event("toggle-all", %{"value" => "on"}, socket) do
+    roles_ids = socket.assigns.all_roles |> Enum.map(& &1.id)
+
+    {:noreply,
+      socket
+        |> stream(:roles, list_roles())
+        |> assign(:toggle_ids, roles_ids)}
+  end
+
+  def handle_event("toggle-all", %{}, socket) do
+      {:noreply,
+      socket
+        |> stream(:roles, list_roles())
+        |> assign(:toggle_ids, [])}
+  end
+
+
+
+  def handle_event("toggle", %{"toggle-id" => id}, socket) do
+    id = String.to_integer(id)
+    toggle_ids = socket.assigns.toggle_ids
+
+    toggle_ids =
+      if (id in toggle_ids) do
+        Enum.reject(toggle_ids, & &1 == id)
+      else
+        [id|toggle_ids]
+      end
+
+    {:noreply, assign(socket, :toggle_ids, toggle_ids)}
+  end
+
+  @impl true
+  def handle_event("delete_selected", _params, socket) do
+    toggle_ids = socket.assigns.toggle_ids
+
+    socket =
+      Enum.reduce(toggle_ids, socket, fn id, acc ->
+        role = Roles.get_role!(id)
+        {:ok, _} = Roles.delete_role(role)
+        stream_delete(acc, :roles, role)
+      end)
+
+    {:noreply, assign(socket, toggle_ids: [])
+         |> put_flash(:info, "Selected roles deleted successfully.")}
+  end
+
+
   @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     role = Roles.get_role!(id)
     {:ok, _} = Roles.delete_role(role)
 
-    {:noreply, stream_delete(socket, :roles, role)}
+    {:noreply, stream_delete(socket, :roles, role)
+               |> put_flash(:info, "Role deleted successfully.")}
+  end
+
+
+  defp list_roles() do
+    Roles.list_roles()
   end
 end
